b54d48cc42e6e4c7e8d0fddbf526b2f2f5694bd9
Valentina <valya.romanchuk3211@gmail.com>
2018-11-21 02:35:56 +0200

    Fixed xml and styleCop_4

--- Task_3-master/Task 3/Task3/Task3/DAO/IOTypes/DriversDB.cs
+++ Task_3-master/Task 3/Task3/Task3/DAO/IOTypes/DriversDB.cs
@@ -1,70 +1,110 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using Task3.DAO.DataTypes;
-
-namespace Taxi_Driver_WPF.IOTypes
+﻿namespace Taxi_Driver_WPF.IOTypes
 {
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using Task3.DAO.DataTypes;
+
+    /// <summary>
+    /// Represents class DriversDB
+    /// </summary>
     public class DriversDB
     {
+        /// <summary>
+        /// Variable fileName
+        /// </summary>
         private string fileName;
+
+        /// <summary>
+        /// List of drivers
+        /// </summary>
         private List<Driver> allDrivers;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DriversDB"/> class with specified fileName
+        /// </summary>
+        /// <param name="fileName">file name</param>
+        public DriversDB(string fileName)
+        {
+            this.allDrivers = new List<Driver>();
+            this.fileName = fileName;
+        }
+
+        /// <summary>
+        /// Gets variable allDrivers
+        /// </summary>
+        /// <value>The allDrivers</value>
         public List<Driver> AllDrivers
         {
             get
             {
-                return allDrivers;
+                return this.allDrivers;
             }
+
             set
             {
                 if (value.Count == 0)
                 {
                     throw new ArgumentOutOfRangeException("DriversDB is empty");
                 }
-                allDrivers = value;
+
+                this.allDrivers = value;
             }
         }
-        public DriversDB(string _fileName)
-        {
-            allDrivers = new List<Driver>();
-            fileName = _fileName;
-        }
 
+        /// <summary>
+        /// Read from file
+        /// </summary>
         public void ReadFromFile()
         {
-            string[] allLines = File.ReadAllLines(fileName);
+            string[] allLines = File.ReadAllLines(this.fileName);
             foreach (string line in allLines)
             {
                 string[] lineElems = line.Split(' ');
-                allDrivers.Add(new Driver(Convert.ToUInt32(lineElems[0]), lineElems[1], lineElems[2], Convert.ToUInt32(lineElems[3]), lineElems[4], Convert.ToUInt32(lineElems[5]), Convert.ToUInt32(lineElems[6]), Convert.ToDouble(lineElems[7])));
+                this.allDrivers.Add(new Driver(Convert.ToUInt32(lineElems[0]), lineElems[1], lineElems[2], Convert.ToUInt32(lineElems[3]), lineElems[4], Convert.ToUInt32(lineElems[5]), Convert.ToUInt32(lineElems[6]), Convert.ToDouble(lineElems[7])));
             }
         }
+
+        /// <summary>
+        /// Update driver
+        /// </summary>
+        /// <param name="driverToUpdate">driver to update</param>
         public void UpdateDriver(Driver driverToUpdate)
         {
-            for (int i = 0; i < allDrivers.Count; ++i)
+            for (int i = 0; i < this.allDrivers.Count; ++i)
             {
-                if (allDrivers[i].Id == driverToUpdate.Id)
+                if (this.allDrivers[i].Id == driverToUpdate.Id)
                 {
-                    allDrivers[i] = driverToUpdate;
+                    this.allDrivers[i] = driverToUpdate;
                     break;
                 }
             }
         }
+
+        /// <summary>
+        /// Write to file
+        /// </summary>
         public void WriteToFile()
         {
-            using (StreamWriter writer = new StreamWriter(fileName))
+            using (StreamWriter writer = new StreamWriter(this.fileName))
             {
-                foreach (Driver driver in allDrivers)
+                foreach (Driver driver in this.allDrivers)
                 {
                     writer.WriteLine(driver);
                 }
             }
         }
 
+        /// <summary>
+        /// Find driver
+        /// </summary>
+        /// <param name="surname">surname</param>
+        /// <param name="name">name</param>
+        /// <returns>search result</returns>
         public Driver FindDriver(string surname, string name)
         {
             Driver searchResult = new Driver();
-            foreach (Driver driver in allDrivers)
+            foreach (Driver driver in this.allDrivers)
             {
                 if (driver.Surname == surname && driver.Name == name)
                 {
@@ -72,13 +112,19 @@ namespace Taxi_Driver_WPF.IOTypes
                     break;
                 }
             }
+
             return searchResult;
         }
 
+        /// <summary>
+        /// Get driver by id
+        /// </summary>
+        /// <param name="driverId">driver id</param>
+        /// <returns>search result</returns>
         public Driver GetDriverById(uint driverId)
         {
             Driver searchResult = new Driver();
-            foreach (Driver driver in allDrivers)
+            foreach (Driver driver in this.allDrivers)
             {
                 if (driver.Id == driverId)
                 {
@@ -86,6 +132,7 @@ namespace Taxi_Driver_WPF.IOTypes
                     break;
                 }
             }
+
             return searchResult;
         }
     }
